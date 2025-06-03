66d0b7befe333a5eee925dd2d820ebac859d76da
Xxdrocks <kerjaanpahri@gmail.com>
2025-06-02 23:13:15 -0700

    gsap animation

--- src/sections/ShowCaseSection.jsx
+++ src/sections/ShowCaseSection.jsx
@@ -1,40 +1,91 @@
 import React from "react";
+import { gsap } from "gsap";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import { expCards } from "../constants";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowCaseSection = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          y: 100,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.5,
+          delay: 0.2 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=100",
+          },
+        }
+      );
+    });
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1 }
+    );
+  }, []);
+
   return (
-    <div id="work" className="app-showcase">
+    <section id="work" ref={sectionRef} className="app-showcase">
       <div className="w-full">
         <div className="showcaselayout">
-            {/* left */}
-            <div className="first-project-wrapper">
-                <div className="image-wrapper">
-                    <img src="/images/project1.png" alt="project" />
-                </div>
-                <div className="text-content">
-                    <h2>On-Demand Books App for Kids and User Friendly App called Bukoo</h2>
-                    <p className="text-white-50 md:text-xl">
-                        an Built App with Laravel and css Native for a fast user-Friendly experience.
-                    </p>
-                </div>
+          {/* left */}
+          <div className="first-project-wrapper" ref={project1Ref}>
+            <div className="image-wrapper">
+              <img src="/images/project1.png" alt="project" />
             </div>
-            {/* right */}
-            <div className="project-list-wrapper overflow-hidden">
-                <div className="project">
-                    <div className="image-wrapper bg-[#ffefdb]">
-                        <img src="/images/project2.png" alt="project"/>
-                    </div>
-                    <h2>On-Demand Books App for Kids and User Friendly App called Bukoo</h2>
-                </div>
-                <div className="project">
-                    <div className="image-wrapper bg-[#ffe7eb]">
-                        <img src="/images/project3.png" alt="project"/>
-                    </div>
-                    <h2>On-Demand Books App for Kids and User Friendly App called Bukoo</h2>
-                </div>
+            <div className="text-content">
+              <h2>
+                On-Demand Books App for Kids and User Friendly App called Bukoo
+              </h2>
+              <p className="text-white-50 md:text-xl">
+                an Built App with Laravel and css Native for a fast
+                user-Friendly experience.
+              </p>
             </div>
+          </div>
+          {/* right */}
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={project2Ref}>
+              <div className="image-wrapper bg-[#ffefdb]">
+                <img src="/images/project2.png" alt="project" />
+              </div>
+              <h2>
+                On-Demand Books App for Kids and User Friendly App called Bukoo
+              </h2>
+            </div>
+            <div className="project" ref={project3Ref}>
+              <div className="image-wrapper bg-[#ffe7eb]">
+                <img src="/images/project3.png" alt="project" />
+              </div>
+              <h2>
+                On-Demand Books App for Kids and User Friendly App called Bukoo
+              </h2>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
