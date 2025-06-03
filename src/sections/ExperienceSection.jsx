8d177cd105cfa2d8b43d0fda958087e1fd5dec26
Xxdrocks <kerjaanpahri@gmail.com>
2025-06-03 05:14:29 -0700

    tambahin experience

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+import { expCards } from "../constants";
 
 const ExperienceSection = () => {
   return (
-    <div>
-      Experience
-    </div>
-  )
-}
+    <section
+      id="experience"
+      className="w-full md:mt-40 mt-20 section-padding xl:px-0 "
+    >
+      <div className="w-full h-full md:px-20 px-5">
+        <TitleHeader title="My Experience" sub="My Career" />
+      </div>
 
-export default ExperienceSection
+      <div className="mt-32 relative">
+        <div className="relative z-50 xl:space-y-32 space-y-10">
+          {expCards.map((card) => (
+            <div key={card.title} className="exp-card-wrapper">
+              {card.title}
+            </div>
+          ))}
+          </div>
+      </div>
+    </section>
+  );
+};
+
+export default ExperienceSection;
