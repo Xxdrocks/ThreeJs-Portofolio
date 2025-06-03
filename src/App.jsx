2ed204cf1bffce9a283740282cf4b31735e206cf
Xxdrocks <kerjaanpahri@gmail.com>
2025-06-03 00:31:21 -0700

    Feature Card Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import LogoSection from './sections/LogoSection.jsx'  
+import NavBar from './components/NavBar.jsx'
+import FeatureCard from './sections/FeatureCard.jsx'
 import Hero from './sections/Hero.jsx'
 import ShowCaseSection from './sections/ShowCaseSection.jsx'
+import ExperienceSection from './sections/ExperienceSection.jsx'
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
+      {/* <LogoSection /> */}
       <ShowCaseSection />
+      <FeatureCard />
+      <ExperienceSection />
     </>
   )
 }
